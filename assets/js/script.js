592152adcfbac0a1d1673901e8a4bd6b98784bcd
ewoigift <2105065@students.kcau.ac.ke>
2024-02-26 18:23:19 +0300

    icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -650,7 +650,7 @@
     /*------------------------------------------
         = BACK TO TOP BTN SETTING
     -------------------------------------------*/
-    $("body").append("<a href='#' class='back-to-top'><i class='ti-arrow-up'></i></a>");
+    $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-up'></i></a>");
 
     function toggleBackToTopBtn() {
         var amountScrolled = 1000;
